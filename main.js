42e0e74529ef75d568a93f7a895bacdeb4b2008e
Lucas Frazzetta <lucasfrazzetta@gmail.com>
2024-07-17 18:48:59 -0300

    Add files via upload
    Subí mis archivos por primera vez

---
+++ main.js
@@ -0,0 +1,10 @@
+const hamburgerIcon = document.getElementById('hamburger-icon');
+const navList = document.getElementById('nav-list');
+
+hamburgerIcon.addEventListener('click', () => toogleDisplay(navList));
+
+const toogleDisplay = element => {
+	element.style.display == 'flex'
+		? (element.style.display = 'none')
+		: (element.style.display = 'flex');
+};
