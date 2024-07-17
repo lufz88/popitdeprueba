cc9d5693aa6d805bb48926197ce66e121b7847e1
Lucas Frazzetta <lucasfrazzetta@gmail.com>
2024-07-17 18:53:38 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 
+const hamburgerIcon = document.getElementById('hamburger-icon');
+const navList = document.getElementById('nav-list');
+
+hamburgerIcon.addEventListener('click', () => toogleDisplay(navList));
+
+const toogleDisplay = element => {
+	element.style.display == 'flex'
+		? (element.style.display = 'none')
+		: (element.style.display = 'flex');
+};
